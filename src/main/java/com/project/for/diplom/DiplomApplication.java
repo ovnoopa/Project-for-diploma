cde946aa053f9dd131afbf7ffe2ce488aa78873e
Сергей Кормилицын <lthemaskl9@gmail.com>
2019-05-06 23:08:40 +0400

    добавил тестовый вариант таблици скрипт добавления элементов в таблицу и тестовый вариант гугл чарта

--- src/main/java/com/project/for/diplom/DiplomApplication.java
+++ src/main/java/com/project/for/diplom/DiplomApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiplomApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DiplomApplication.class, args);
 	}
