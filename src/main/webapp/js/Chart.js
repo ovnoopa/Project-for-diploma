3da024966dc3af6c27ae1f99f3e9abe35652c8be
Сергей Кормилицын <lthemaskl9@gmail.com>
2019-05-20 23:59:51 +0400

    миниправкидлянаеба

--- src/main/webapp/js/Chart.js
+++ src/main/webapp/js/Chart.js
@@ -3,16 +3,20 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Время', 'Пинг'],
-    ['20:12',  63,],
-    ['20:13',  70],
-    ['20:14',  56],
-    ['20:15',  44]
+    ['Время', 'Ping'],
+    ['20:10',  169,],
+    ['20:05',  151],
+    ['20:00',  130],
+    ['19:55',  176],
+    ['19:50',  152],
+    ['19:45',  147],
+    ['19:40',  132],
+    ['19:35',  121]
   ]);
 
   var options = {
-    title: 'Пинг камеры',
-    hAxis: {title: 'Камера №1',  titleTextStyle: {color: '#333'}},
+    title: 'Ping устройства',
+    hAxis: {title: 'Camera1',  titleTextStyle: {color: '#333'}},
     vAxis: {minValue: 0}
   };
 
